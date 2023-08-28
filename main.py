70dd82f27bc0bd10772d587452bb0f5893b25dde
HENRY亨利 <henry@thinkerx.com>
2023-08-28 16:38:34 +0800

    feat: scripts

---
+++ main.py
@@ -0,0 +1,12 @@
+import platform
+import asyncio
+
+
+async def start():
+    print("START")
+
+
+if __name__ == '__main__':
+    if platform.system() == 'Windows':
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(start())
